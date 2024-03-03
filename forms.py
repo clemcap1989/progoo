73d00f9e84e9618eb711e67be2f98c43e7fefb7d
Clément Barré <cbarre@quantmetry.com>
2024-03-03 15:31:41 +0100

    registration

--- forms.py
+++ forms.py
@@ -1,5 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Length, Email
 
 class RegistrationForm(FlaskForm):
-    username =  StringField('Username')
+    username =  StringField('Username', 
+                            validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', 
+                                     validators=[DataRequired()])
