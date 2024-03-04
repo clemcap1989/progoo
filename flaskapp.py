b82e7db8e33c26be5df52c94b118fc79bf1fdd58
Clément Barré <cbarre@quantmetry.com>
2024-03-04 10:23:36 +0100

    space

--- flaskapp.py
+++ flaskapp.py
@@ -16,9 +16,8 @@ class User(db.Model):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
-
 def __repr__(self):
-    return f"User('{self.username}, {self.email}, {self.image_file})"
+    return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,8 +27,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 def __repr__(self):
-    return f"Post('{self.title}, {self.date_posted})"
-
+    return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
