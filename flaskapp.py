476abd66120050bae36c9370d6eaa4094327aa3c
Clément Barré <cbarre@quantmetry.com>
2024-03-04 10:20:41 +0100

    app

--- flaskapp.py
+++ flaskapp.py
@@ -11,7 +11,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    username = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
