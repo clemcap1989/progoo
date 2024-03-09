a12f21bca8b4e94b88c418e45a6e263118f860ce
Clément Barré <cbarre@quantmetry.com>
2024-03-09 13:37:03 +0100

    nouveau

--- flaskapp.py
+++ flaskapp.py
@@ -15,27 +15,6 @@ db.init_app(app)
 
 app.app_context().push()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-
-def __repr__(self):
-    return '<User %r>' % self.username
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-def __repr__(self):
-    return f"Post('{self.title}', '{self.date_posted}')"
-
 with app.app_context():
     db.create_all()
 
