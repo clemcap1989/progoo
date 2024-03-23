56326735890595319145103a7a30f4eb2615401b
Clement Barre <clement.barre@capgemini.com>
2024-03-23 18:52:45 +0100

    Login

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskapp.models import User
 
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
@@ -22,6 +23,7 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
