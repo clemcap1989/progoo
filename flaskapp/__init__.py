85f1139753913d624b5f21cbefbf50d5f5527a04
Clement Barre <clement.barre@capgemini.com>
2024-03-23 19:10:47 +0100

    Layout

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
+from flask_login import LoginManager
 
 # create the extension
 db = SQLAlchemy()
@@ -18,5 +18,9 @@ app.app_context().push()
 
 with app.app_context():
     db.create_all()
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskapp import routes
